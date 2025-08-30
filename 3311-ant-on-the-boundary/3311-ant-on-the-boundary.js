d33d6958aeb91d0e462d2e3743e9e969ab250408
Melvin-James <melvinjamesk2003@gmail.com>
2025-08-30 12:33:00 +0530

    Time: 45 ms (62.86%), Space: 53.8 MB (76.57%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function(nums) {
+    let location=0;
+    let count =0;
+    for(let i=0;i<nums.length;i++){
+        location+=nums[i];
+        if(location===0){
+            count++;
+        }
+    }
+    if(count){
+        return count;
+    }else{
+        return 0;
+    }
+};
