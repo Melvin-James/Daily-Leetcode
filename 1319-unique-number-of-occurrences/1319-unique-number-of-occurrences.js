77c605af3ac9d0d5ef3e08e25f232e1accaabc4f
Melvin-James <melvinjamesk2003@gmail.com>
2025-09-23 16:55:22 +0530

    Time: 3 ms (16.86%), Space: 55.1 MB (30.68%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let occurance = {};
+    for(let ele of arr){
+        occurance[ele]=(occurance[ele] || 0)+1;
+    }
+    let array=Object.values(occurance);
+    let res = array.filter((ele,index,array)=>{return array.indexOf(ele)!==index});
+    let count=0;
+    let c = res.map((ele)=>count++);
+    if(count===0){
+        return true;
+    }else{
+        return false;
+    }
+};
