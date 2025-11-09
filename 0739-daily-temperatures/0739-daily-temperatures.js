1f96073fad2efcb69f593624fcab99ce1afe17a3
MELVIN JAMES <melvinjamesk2003@gmail.com>
2025-11-09 20:50:47 +0530

    Time: 13 ms (96.19%), Space: 77.1 MB (48.32%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let stack = [];
+    let answer =new Array(temperatures.length).fill(0);
+    for(let i=0;i<temperatures.length;i++){
+        while(stack.length && temperatures[i]>temperatures[stack[stack.length-1]]){
+            let prevIndex = stack.pop();
+            answer[prevIndex] = i - prevIndex;
+        }
+        stack.push(i);
+    }
+    return answer;
+};
