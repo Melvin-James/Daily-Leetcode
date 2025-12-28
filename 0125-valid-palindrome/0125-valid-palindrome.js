37511d9a577dd6d1123f8aa8c7a8d6a3ed1ecf66
MELVIN JAMES <melvinjamesk2003@gmail.com>
2025-12-28 12:33:27 +0530

    Time: 5 ms (62.48%), Space: 57.8 MB (65.33%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,10 +4,14 @@
  */
 var isPalindrome = function(s) {
     let t = s.toLowerCase().replace(/[^a-z0-9]/g,'');
-    let r = t.split('').reverse().join('');
-    if(t===r){
-        return true;
-    }else{
-        return false;
+    let left = 0;
+    let right = t.length-1;
+    while(left<right){
+        if(t[left]!==t[right]){
+            return false;
+        }
+        left++;
+        right--;
     }
+    return true;
 };
