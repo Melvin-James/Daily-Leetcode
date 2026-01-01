66420e7e2ca8823e119ed91c4d1162bdbca473cf
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-01-01 18:49:56 +0530

    Time: 0 ms (100%), Space: 53.7 MB (82.28%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -1,9 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
 var removeElement = function(nums, val) {
     let pointer = 0;
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==val){
-           nums[pointer] = nums[i];
-           pointer++;
+            nums[pointer] = nums[i];
+            pointer++;
         }
     }
     return pointer;
