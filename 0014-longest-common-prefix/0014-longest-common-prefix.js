dcc4ee519ec855a2fb51e0f638331a17c2e8e34f
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-01-14 11:55:54 +0530

    Time: 6 ms (13.4%), Space: 57.1 MB (13.13%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+class Node{
+    constructor(){
+        this.children = {};
+        this.isWordEnd = false;
+    }
+}
+class Trie{
+    constructor(){
+        this.root = new Node();
+    }
+    insert(word){
+        let current = this.root;
+        for(let ch of word){
+            if(!current.children[ch]){
+                current.children[ch] = new Node();
+            }
+            current = current.children[ch];
+        }
+        current.isWordEnd = true;
+    }
+    longestCommonPrefix(){
+        let prefix = "";
+        let node = this.root;
+        while(true){
+            let keys = Object.keys(node.children);
+            if(keys.length!==1 || node.isWordEnd) break;
+            let char = keys[0];
+            prefix+=char;
+            node = node.children[char];
+        }
+        return prefix;
+    }
+}
+var longestCommonPrefix = function(strs) {
+    if(strs.length===0) return "";
+    const trie = new Trie();
+    for(let str of strs){
+        trie.insert(str);
+    }
+    return trie.longestCommonPrefix();
+};
