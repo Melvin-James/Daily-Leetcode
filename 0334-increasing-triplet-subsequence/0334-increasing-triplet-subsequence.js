46fd205439aa0a4fdccb8edfe010b5c913a0bdff
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-02-27 09:33:40 +0530

    Time: 0 ms (100%), Space: 73.5 MB (89.23%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -0,0 +1,14 @@
+var increasingTriplet = function(nums) {
+    let first = Infinity;
+    let second = Infinity;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]<=first){
+            first = nums[i];
+        }else if(nums[i]<=second){
+            second = nums[i];
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
