9f5b6e6329b17b2efedf66c20670182b8b33f2e9
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-04-17 19:51:04 +0530

    Time: 80 ms (37.71%), Space: 65.6 MB (83.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,29 +4,6 @@
  * @return {number}
  */
 var findKthLargest = function (nums,k){
-    let n = nums.length;
-    for(let i = Math.floor((n/2)-1);i>=0;i--){
-        heapifyDown(nums,n,i);
-    }
-    for (let end = n - 1; end > 0; end--) {
-        [nums[0], nums[end]] = [nums[end], nums[0]];
-        heapifyDown(nums, end, 0);
-    }
-    return nums[nums.length-k];
-}
-function heapifyDown(nums,size,index){
-        let largest = index;
-        let left = 2 * index + 1;
-        let right = 2 * index + 2;
-        if(left < size && nums[left] > nums[largest]){
-            largest = left;
-        }
-        if(right < size && nums[right] > nums[largest]){
-            largest = right;
-        }
-        if(largest !== index){
-            [nums[index],nums[largest]]=
-            [nums[largest],nums[index]];
-            heapifyDown(nums,size,largest);
-        }
+    nums.sort((a,b)=>b-a);
+    return nums[k-1];
 }
