62967de6a4485cb39263022e0ea658cbf05ac2d6
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-07-08 15:51:34 +0530

    Time: 35 ms (33.8%), Space: 85.6 MB (22.13%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,12 +4,14 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    const indexMap = new Map();
+    let seen = new Map();
     for(let i=0;i<nums.length;i++){
-        if(indexMap.has(nums[i])&& (i-indexMap.get(nums[i])<=k)){
-            return true;
+        if(seen.has(nums[i])){
+            if(Math.abs(seen.get(nums[i])-i)<=k){
+                return true;
+            }
         }
-        indexMap.set(nums[i],i);
+        seen.set(nums[i],i);
     }
     return false;
 };
