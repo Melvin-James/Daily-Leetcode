8af314313402f103903dba4a9a73a1a7bf1f5481
MELVIN JAMES <melvinjamesk2003@gmail.com>
2026-07-12 22:25:18 +0530

    Time: 0 ms (100%), Space: 53.6 MB (50.27%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,11 +10,11 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let fast = head;
     let slow = head;
+    let fast = head;
     while(fast && fast.next){
-        fast = fast.next.next;
         slow = slow.next;
+        fast = fast.next.next;
     }
     return slow;
 };
