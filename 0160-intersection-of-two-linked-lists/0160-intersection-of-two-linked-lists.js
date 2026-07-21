b6e938efab9a540c109a2304775111525da1479d
melvinjamesk <melvinjamesk2003@gmail.com>
2026-07-21 09:33:16 +0530

    Time: 63 ms (76.06%), Space: 66.9 MB (52.94%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,12 +12,11 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    if(!headA || !headB) return null;
-    let p1 = headA;
-    let p2 = headB;
-    while(p1 !== p2){
-        p1 = p1 === null ? headB : p1.next;
-        p2 = p2 === null ? headA : p2.next;
+    let pA = headA;
+    let pB = headB;
+    while(pA!==pB){
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
     }
-    return p1;
+    return pA;
 };
