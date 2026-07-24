fd036fce806c5bb0d6ab06bcde255152d6809763
melvinjamesk <melvinjamesk2003@gmail.com>
2026-07-24 15:49:15 +0530

    Time: 0 ms (100%), Space: 53.2 MB (85.51%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let res = [];
+    function preOrder(root){
+        if(root){
+            res.push(root.val);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+    preOrder(root);
+    return res;
+};
