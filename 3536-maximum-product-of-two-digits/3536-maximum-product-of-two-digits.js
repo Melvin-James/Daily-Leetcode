00f371221aa73feb448c804d94e39153dd7c374a
melvinjamesk <melvinjamesk2003@gmail.com>
2026-07-25 10:05:16 +0530

    Time: 2 ms (57.45%), Space: 57.1 MB (36.17%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -3,14 +3,9 @@
  * @return {number}
  */
 var maxProduct = function(n) {
-    let max = 0;
-    let sum = 0;
-    let s = n.toString();
-    for(let i=0;i<s.length-1;i++){
-        for(let j=i+1;j<s.length;j++){
-            sum = Number(s[i])*Number(s[j]);
-            max = Math.max(sum,max);
-        }
-    }
-    return max;
+    n = String(n).split('');
+    let sort = n.sort((a,b)=>b-a);
+    let n1 = sort[0];
+    let n2 = sort[1];
+    return n1 * n2;
 };
