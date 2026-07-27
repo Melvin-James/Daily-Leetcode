d4d9b277b2a17329140a6908720d2008aeaa17c1
melvinjamesk <melvinjamesk2003@gmail.com>
2026-07-27 09:37:16 +0530

    Time: 2 ms (12.86%), Space: 57.6 MB (51.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -12,17 +12,21 @@
  */
 var mergeTwoLists = function(list1, list2) {
     let dummy = new ListNode(-1);
-    let current = dummy;
+    let tail = dummy;
     while(list1 && list2){
-        if(list1.val <= list2.val){
-            current.next = list1;
-            list1 = list1.next;
+        if(list1.val < list2.val){
+            tail.next = list1;
+            list1=list1.next;
         }else{
-            current.next = list2;
-            list2 = list2.next;
+            tail.next=list2;
+            list2=list2.next;
         }
-        current = current.next;
+        tail = tail.next;
     }
-    current.next = list1 ? list1 : list2;
+    if(list1){
+        tail.next = list1;
+    }else{
+        tail.next = list2;
+    }
     return dummy.next;
 };
