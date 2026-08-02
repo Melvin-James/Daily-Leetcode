473679c2137fc9f4e00a3248fc12b3c6b9a7c45d
melvinjamesk <melvinjamesk2003@gmail.com>
2026-08-02 23:21:20 +0530

    Time: 0 ms (100%), Space: 52.8 MB (93.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,16 +5,15 @@
  */
 var searchInsert = function(nums, target) {
     let low = 0;
-    let high = nums.length-1;
-    let mid = 0;
-    while(low<=high){
-        mid = Math.floor((low+high)/2);
+    let high = nums.length - 1;
+    while(low <= high){
+        let mid = Math.floor((low+high)/2);
         if(nums[mid]===target){
             return mid;
         }else if(target < nums[mid]){
-            high=mid-1
+            high = mid - 1;
         }else{
-            low=mid+1;
+            low = mid + 1;
         }
     }
     return low;
