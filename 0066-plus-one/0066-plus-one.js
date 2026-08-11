5625b463668b62badf9830a048da777a65f7595a
melvinjamesk <melvinjamesk2003@gmail.com>
2026-08-11 20:10:56 +0530

    Time: 0 ms (100%), Space: 53.2 MB (80.19%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,14 +3,6 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for(let i=digits.length-1;i>=0;i--){
-        if(digits[i]<9){
-            digits[i]++;
-            return digits;
-        }
-        digits[i]=0;
-    }
-
-    digits.unshift(1);
-    return digits;
+    let num = BigInt(digits.join('')) + 1n;
+    return num.toString().split('').map(Number);
 };
