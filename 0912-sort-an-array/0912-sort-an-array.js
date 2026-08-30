e0d4cb3e5351c6fed863d2ec96b5de727e700be0
melvinjamesk <melvinjamesk2003@gmail.com>
2026-08-30 23:02:21 +0530

    Time: 895 ms (5.08%), Space: 82.5 MB (18.67%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    if(nums.length < 2){
+        return nums;
+    }
+    let mid = Math.floor(nums.length/2);
+    let left = nums.slice(0,mid);
+    let right = nums.slice(mid);
+    return merge(sortArray(left),sortArray(right));
+};
+
+var merge = function(left,right){
+    let sorted = [];
+    while(left.length && right.length){
+        if(left[0] <= right[0]){
+            sorted.push(left.shift());
+        }else{
+            sorted.push(right.shift());
+        }
+    }
+    return [...sorted,...left,...right];
+}
